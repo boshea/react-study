810203c6dc2a1987403e2feca1859a3d33aff63e
Brian <boshea@salesforce.com>
2018-06-29 14:46:38 -0700

    Replaced annoying tabs with spaces.

--- react-getting-started/cardlist.jsx
+++ react-getting-started/cardlist.jsx
@@ -1,19 +1,19 @@
 const Card = (props) => {
-	return (
-  	<div>
-    	<img width="75" src={props.avatar_url}/>
+  return (
+    <div>
+      <img width="75" src={props.avatar_url}/>
       <div style={{display: 'inline-block', marginLeft: 10}}>
-      	<div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
-      	<div>{props.company}</div>
+        <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
+        <div>{props.company}</div>
       </div>
     </div>
   )
 }
 
 const CardList = (props) => {
-	return (
-  	<div>
-    	{props.cards.map(card => <Card key={card.id} {...card}/>)}
+  return (
+    <div>
+      {props.cards.map(card => <Card key={card.id} {...card}/>)}
     </div>
   );
 }
@@ -23,32 +23,32 @@ class Form extends React.Component {
     userName: ''
   };
 
-	handleSubmit = (event) => {
-  	event.preventDefault();
+  handleSubmit = (event) => {
+    event.preventDefault();
     // console.log("Event: Form Submit: " + this.state.userName);
 
-		// XXX: Possible security vulnerability: we could enter any data we want into the form,
+    // XXX: Possible security vulnerability: we could enter any data we want into the form,
     // and this will just get blindly appended to the URL and passed to the axios.get() call.
     //
     // axios.get('https://api.github.com/users/${this.state.userName}')
-    //	.then(resp => { console.log(resp) });
+    //  .then(resp => { console.log(resp) });
 
     axios.get("https://api.github.com/users/" + this.state.userName)
-    	.then(resp => {
-      	this.props.onSubmit(resp.data);
+      .then(resp => {
+        this.props.onSubmit(resp.data);
         this.setState({ userName: '' });
       })
       .catch(error => {
-      	console.log("No such user: " + this.state.userName);
-			}
-		);
+        console.log("No such user: " + this.state.userName);
+      }
+    );
   }
 
-	render() {
-  	return (
-    	<form onSubmit={this.handleSubmit}>
-      	<input
-        	type="text"
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
           value={this.state.userName}
           onChange={(event) => this.setState({ userName: event.target.value })}
           placeholder="GitHub username"
@@ -63,20 +63,20 @@ class App extends React.Component {
   // Consider making this an object and using it as a map of username to card info.
   // This would get rid of the problem of duplicates.  Also consider sorting the cards
   // by name to make it easier to look them up in the card list.
-	state = {
-  	cards: [ ]
+  state = {
+    cards: [ ]
   };
   
   addNewCard = (cardInfo) => {
     this.setState(
-    	prevState => ( { cards: prevState.cards.concat(cardInfo) } )
+      prevState => ( { cards: prevState.cards.concat(cardInfo) } )
     );
   };
 
-	render() {
-  	return (
-    	<div>
-      	<Form onSubmit={this.addNewCard} />
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.addNewCard} />
         <CardList cards={this.state.cards}/>
       </div>
     );
